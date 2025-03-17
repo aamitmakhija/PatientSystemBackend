92a0fd8d991b3f08259c504ac5c285bbabe0b2ff
Grizzlyrj <166296702+Grizzlyrj@users.noreply.github.com>
2025-03-17 23:45:55 +0400

    Remove registrationType field from Patient model

--- models/Admission.js
+++ models/Admission.js
@@ -5,17 +5,12 @@ const AdmissionSchema = new mongoose.Schema(
     {
         patientId: {
             type: mongoose.Schema.Types.ObjectId,
-<<<<<<< HEAD
             ref: 'Patient',  //  Referring to the Patient model
             required: [true, 'Patient ID is required'],  // Ensure patient ID is provided
-=======
-            ref: 'Patient',  
-            required: [true, 'Patient ID is required'],  
->>>>>>> e40f0174dfad25093bc2c05f51f257bb035bad20
+ 
         },
         wardType: {
             type: String,
-<<<<<<< HEAD
             required: [true, 'Ward is required'],  // Ensure ward is specified
             enum: ['Medicine A',       'Medicine B',       'Medicine C',       'Medicine D',
                 'Surgery A',        'Surgery B',        'Surgery C',        'Surgery D', 
@@ -27,27 +22,15 @@ const AdmissionSchema = new mongoose.Schema(
                 'Dermatology A',    'Dermatology B',    'Dermatology C',    'Dermatology D',
                 'Oncology A',       'Oncology B',       'Oncology C',       'Oncology D',
                 'ICU',              'CCU'],  // Allowed ward types
-=======
-            required: [true, 'Ward is required'],  
-            enum: ['ICU', 'General', 'Pediatrics', 'Surgery'],  
->>>>>>> e40f0174dfad25093bc2c05f51f257bb035bad20
+            required: [true, 'Ward is required'],   
         },
         doctorAssigned:{
             type: String,
-<<<<<<< HEAD
-            required: true,
-            trim:true, // Trims the doctors name without spaces
-=======
             trim: true,  
->>>>>>> e40f0174dfad25093bc2c05f51f257bb035bad20
         },
         dateOfAdmission:{
             type: Date,
-<<<<<<< HEAD
-            default: Date.now, // Set default value to current time
-=======
             default: Date.now,  
->>>>>>> e40f0174dfad25093bc2c05f51f257bb035bad20
         },
         dischargedAt: {
             type: Date,
